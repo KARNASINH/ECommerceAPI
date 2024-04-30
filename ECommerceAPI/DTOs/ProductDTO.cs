5c9cbe62372c6a1e981a4c52ca83acfe25913358
Karnasinh <karnasinh281@gmail.com>
2024-04-30 10:04:31 -0400

    Creates ProductResponseDTO class to return newly created ProductID in the reponse.

--- ECommerceAPI/DTOs/ProductDTO.cs
+++ ECommerceAPI/DTOs/ProductDTO.cs
@@ -1,7 +1,16 @@
-﻿namespace ECommerceAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceAPI.DTOs
 {
     public class ProductDTO
     {
-
+        public int ProductId { get; set; } // Used only for updates, not for inserts
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        public string Description { get; set; }
     }
 }
