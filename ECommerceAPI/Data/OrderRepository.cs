b46a78e323ecfcde7b77eb6b3c5d4664843d5256
Karnasinh <karnasinh281@gmail.com>
2024-04-30 14:15:04 -0400

    Adds GetAllOrdersAsync() method to fetch the Order details from Database.

--- ECommerceAPI/Data/OrderRepository.cs
+++ ECommerceAPI/Data/OrderRepository.cs
@@ -11,5 +11,45 @@ namespace ECommerceAPI.Data
         {
             _connectionFactory = connectionFactory;
         }
+
+        //This method will fetch the Order details based on the passed Status.
+        public async Task<List<Order>> GetAllOrdersAsync(string Status)
+        {
+            var orders = new List<Order>();
+
+            //T-SQL query to fetch the data from Database
+            var query = "SELECT OrderId, CustomerId, TotalAmount, Status, OrderDate FROM Orders WHERE Status = @Status";
+            
+            using (var connection = _connectionFactory.CreateConnection())
+            {
+                await connection.OpenAsync();
+             
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Status", Status);
+                
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        //Looping through all the Orders
+                        while (await reader.ReadAsync())
+                        {
+                            var order = new Order
+                            {
+                                OrderId = reader.GetInt32(reader.GetOrdinal("OrderId")),
+                                CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                                TotalAmount = reader.GetDecimal(reader.GetOrdinal("TotalAmount")),
+                                Status = reader.GetString(reader.GetOrdinal("Status")),
+                                OrderDate = reader.GetDateTime(reader.GetOrdinal("OrderDate"))
+                            };
+                    
+                            //Adding fetched order to the List of Order
+                            orders.Add(order);
+                        }
+                    }
+                }
+            }
+            //Return order
+            return orders;
+        }
     }
 }
