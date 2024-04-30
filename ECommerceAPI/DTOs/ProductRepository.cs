72032b421aabe35b4d7ea1e8e927cc9e0deb7b84
Karnasinh <karnasinh281@gmail.com>
2024-04-30 10:39:38 -0400

    Add a GetProductByIdAsync() method which return the product based on the specified ProductId from the database.

--- ECommerceAPI/DTOs/ProductRepository.cs
+++ ECommerceAPI/DTOs/ProductRepository.cs
@@ -48,5 +48,44 @@ namespace ECommerceAPI.DTOs
             //Returns the all products
             return products;
         }
+
+        //This method serch product based on the given Id and return all the data for that Product.
+        public async Task<Product?> GetProductByIdAsync(int productId)
+        {
+            //T-SQL query to fetch Product if the product exists in the database
+            var query = "SELECT ProductId, Name, Price, Quantity, Description, IsDeleted FROM Products WHERE ProductId = @ProductId AND              IsDeleted = 0";
+            
+            Product? product = null;
+            
+            using (var connection = _connectionFactory.CreateConnection())
+            {
+                await connection.OpenAsync();
+            
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ProductId", productId);
+                    
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        if (await reader.ReadAsync())
+                        {
+                            product = new Product
+                            {
+                                //GetOrdinal method fetch the data for a defined column from a raw.
+                                ProductId = reader.GetInt32(reader.GetOrdinal("ProductId")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Price = reader.GetDecimal(reader.GetOrdinal("Price")),
+                                Quantity = reader.GetInt32(reader.GetOrdinal("Quantity")),
+                                Description = reader.GetString(reader.GetOrdinal("Description")),
+                                IsDeleted = reader.GetBoolean(reader.GetOrdinal("IsDeleted"))
+                            };
+                        }
+                    }
+                }
+            }
+
+            //Returns the product.
+            return product;
+        }
     }
 }
