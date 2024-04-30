7aed77bdc407a657dcb7eb82f61ef0e4eebd054c
Karnasinh <karnasinh281@gmail.com>
2024-04-30 12:37:30 -0400

    Adds CreateOrderResponseDTO class to hold the Order infromation when User places the Order.

---
+++ ECommerceAPI/DTOs/CreateOrderResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceAPI.DTOs
+{
+    //This class holds the information when User place the order.
+    public class CreateOrderResponseDTO
+    {
+        public int OrderId { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public bool IsCreated { get; set; }
+    }
+}
