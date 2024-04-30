439233202055985106514309706f38c3537b2004
Karnasinh <karnasinh281@gmail.com>
2024-04-29 23:56:36 -0400

    Created CustomerResponseDTO.cs class and add the property.

---
+++ ECommerceAPI/DTOs/CustomerResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerceAPI.DTOs
+{
+    //We will use this class to return the newly created Customer ID to the User.
+    public class CustomerResponseDTO
+    {
+        public int CustomerId { get; set; }
+    }
+}
