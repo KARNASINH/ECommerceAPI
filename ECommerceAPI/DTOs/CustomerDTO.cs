a85e1b36719823609da136e0c41830039746dfa0
Karnasinh <karnasinh281@gmail.com>
2024-04-29 22:58:55 -0400

    Created CustomerDTO.cs class and added properties.

---
+++ ECommerceAPI/DTOs/CustomerDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceAPI.DTOs
+{
+    public class CustomerDTO
+    {
+        public int CustomerId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
