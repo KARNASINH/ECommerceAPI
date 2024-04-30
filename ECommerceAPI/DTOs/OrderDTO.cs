e87696325e2f7c603c995c1c81ab00bc07d2f98b
Karnasinh <karnasinh281@gmail.com>
2024-04-30 12:13:17 -0400

    Adds a OrderDTO class to represent details about Order placed.

---
+++ ECommerceAPI/DTOs/OrderDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceAPI.DTOs
+{
+    //This class represents details about Order placed
+    public class OrderDTO
+    {
+        [Required]
+        public int CustomerId { get; set; }
+        [Required]
+        public List<OrderItemDetailsDTO> Items { get; set; }
+    }
+}
