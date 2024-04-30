6e4303f5e8c1d1d23582ce0bafbb458a3fab9eaa
Karnasinh <karnasinh281@gmail.com>
2024-04-29 20:55:19 -0400

    Added Product.cs class to manage Product data.

--- ECommerceAPI/Models/Customer.cs
+++ ECommerceAPI/Models/Customer.cs
@@ -2,5 +2,10 @@
 {
     public class Customer
     {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
