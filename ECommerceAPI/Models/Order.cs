653465beb62719b3e1f0dca2223dce9246fcc9f6
Karnasinh <karnasinh281@gmail.com>
2024-04-29 21:04:03 -0400

    Added properties to Order.cs class.

--- ECommerceAPI/Models/Order.cs
+++ ECommerceAPI/Models/Order.cs
@@ -2,5 +2,10 @@
 {
     public class Order
     {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; } = "Pending";
+        public DateTime OrderDate { get; set; }
     }
 }
