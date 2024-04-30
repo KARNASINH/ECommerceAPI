079932b83df95eb4601202e1ad74b2f7980f3ea0
Karnasinh <karnasinh281@gmail.com>
2024-04-30 12:21:59 -0400

    Adds OrderStatusDTO class to represents Order Status.

---
+++ ECommerceAPI/DTOs/OrderStatusDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceAPI.DTOs
+{
+    //This class represents Order status details.
+    public class OrderStatusDTO
+    {
+        [Required]
+        public int OrderId { get; set; }
+        [Required]
+        public string Status { get; set; }
+    }
+}
