adb95ca69bacff56b268e713f4a70f5fab0fa7ce
Karnasinh <karnasinh281@gmail.com>
2024-04-30 12:25:29 -0400

    Adds OrderStatusResponseDTO class to store the Order Status Response information

---
+++ ECommerceAPI/DTOs/OrderStatusResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceAPI.DTOs
+{
+    //This class stores the Order status Response infromation whenver we update Order Status.
+    public class OrderStatusResponseDTO
+    {
+        public int OrderId { get; set; }
+        public string Status { get; set; }
+        public bool IsUpdated { get; set; }
+        public string Message { get; set; }
+    }
+}
