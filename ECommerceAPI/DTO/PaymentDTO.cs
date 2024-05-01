1a9a4749d5e8559b1640b10aee8a5159d5327b0d
Karnasinh <karnasinh281@gmail.com>
2024-05-01 01:27:25 -0400

    Adds PaymentDTO class to hold the Payment details based on the Order Id.

---
+++ ECommerceAPI/DTO/PaymentDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceAPI.DTO
+{
+    //This class holds the Payment details based on Order Id.
+    public class PaymentDTO
+    {
+        [Required]
+        public int OrderId { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        public string PaymentType { get; set; }
+    }
+}
