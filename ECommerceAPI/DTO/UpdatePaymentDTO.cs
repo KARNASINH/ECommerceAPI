0830c35003a7c92a5f714e9425b41eb0e9df05fc
Karnasinh <karnasinh281@gmail.com>
2024-05-01 18:24:47 -0400

    Adds UpdatePaymentResponseDTO class to hold the information while updating the Payment Status for the specific order in the Database.

--- ECommerceAPI/DTO/UpdatePaymentDTO.cs
+++ ECommerceAPI/DTO/UpdatePaymentDTO.cs
@@ -2,6 +2,7 @@
 
 namespace ECommerceAPI.DTO
 {
+    //This class update the Payment for the specific Order in the Database.
     public class UpdatePaymentDTO
     {
         [Required]
---
+++ ECommerceAPI/DTO/UpdatePaymentResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerceAPI.DTO
+{
+    //This class hold the information about the Payment Status updation process.
+    public class UpdatePaymentResponseDTO
+    {
+        public int PaymentId { get; set; }
+        public string CurrentStatus { get; set; }
+        public string UpdatedStatus { get; set; }
+        public string Message { get; set; }
+        public bool IsUpdated { get; set; }
+    }
+}
