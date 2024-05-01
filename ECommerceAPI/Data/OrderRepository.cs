57e92c091b38769fb4aef92854cd43c9ccd0ce45
Karnasinh <karnasinh281@gmail.com>
2024-04-30 20:10:42 -0400

    Adds CreateOrderAsync() method which creates an order with the pending state in the Database.

--- ECommerceAPI/Data/OrderRepository.cs
+++ ECommerceAPI/Data/OrderRepository.cs
@@ -51,5 +51,130 @@ namespace ECommerceAPI.Data
             //Return order
             return orders;
         }
+
+        //Create the Order with Pending State
+        public async Task<CreateOrderResponseDTO> CreateOrderAsync(OrderDTO orderDto)
+        {
+            //T-SQL query to fetch the Product details from the database table
+            var productQuery = "SELECT ProductId, Price, Quantity FROM Products WHERE ProductId = @ProductId AND IsDeleted = 0";          
+            
+            //Sotring Total Order Value
+            decimal totalAmount = 0m;
+            
+            //Adding Product in the List if it has passed all validation good to put into the Order
+            List<OrderItem> validatedItems = new List<OrderItem>();
+            
+            CreateOrderResponseDTO createOrderResponseDTO = new CreateOrderResponseDTO();
+            
+            using (var connection = _connectionFactory.CreateConnection())
+            {
+                await connection.OpenAsync();
+                
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        //This will loop through all the Items mentioend in the Order and will calculate the total Order amount
+                        foreach (OrderItemDetailsDTO item in orderDto.Items)
+                        {
+                            using (var productCommand = new SqlCommand(productQuery, connection, transaction))
+                            {
+                                productCommand.Parameters.AddWithValue("@ProductId", item.ProductId);
+                                
+                                //Fetches the Product details
+                                using (var reader = await productCommand.ExecuteReaderAsync())
+                                {
+                                    //This returns true if it has Product data row
+                                    if (await reader.ReadAsync())
+                                    {
+                                        //Fetches product's Quantity
+                                        int stockQuantity = reader.GetInt32(reader.GetOrdinal("Quantity"));
+
+                                        //Fetches product's price
+                                        decimal price = reader.GetDecimal(reader.GetOrdinal("Price"));
+                                        
+                                        //Checking the Product quantity in the Order is greater or equal to the Quantity available in the stock
+                                        if (stockQuantity >= item.Quantity)
+                                        {
+                                            //Total amount for a perticular product in the Order
+                                            totalAmount += price * item.Quantity;
+
+                                            //This will add the Product along with Qunatity in to Order, if are available and sufficient in stock
+                                            validatedItems.Add(new OrderItem
+                                            {
+                                                ProductId = item.ProductId,
+                                                Quantity = item.Quantity,
+                                                PriceAtOrder = price  //This is total price E.g. 2 Nos of Apple with price of 10$ >> 2*10 = 20$
+                                            });
+                                        }
+                                        else
+                                        {
+                                            //Handle the case where there isn't enough stock
+                                            createOrderResponseDTO.Message = $"Insufficient Stock for Product ID {item.ProductId}";
+                                            createOrderResponseDTO.IsCreated = false;
+                                            return createOrderResponseDTO;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        //Handle the case for Invalid Product Id
+                                        createOrderResponseDTO.Message = $"Product Not Found for Product ID {item.ProductId}";
+                                        createOrderResponseDTO.IsCreated = false;
+                                        return createOrderResponseDTO;
+                                    }
+                                    reader.Close(); //Ensure the reader is closed before next iteration to save the resources
+                                }
+                            }
+                        }
+
+                        //T-SQL query to insert the order and to keep a track of which OrderId is inserted into the table
+                        var orderQuery = "INSERT INTO Orders (CustomerId, TotalAmount, Status, OrderDate) OUTPUT INSERTED.OrderId VALUES (@CustomerId, @TotalAmount, @Status, @OrderDate)";
+
+                        //T-SQL query to insert the OrderItems details
+                        var itemQuery = "INSERT INTO OrderItems (OrderId, ProductId, Quantity, PriceAtOrder) VALUES (@OrderId, @ProductId, @Quantity, @PriceAtOrder)";
+
+
+                        //Proceed with creating the order if all items are validated and product has enough stock
+                        using (var orderCommand = new SqlCommand(orderQuery, connection, transaction))
+                        {
+                            orderCommand.Parameters.AddWithValue("@CustomerId", orderDto.CustomerId);
+                            orderCommand.Parameters.AddWithValue("@TotalAmount", totalAmount);
+                            orderCommand.Parameters.AddWithValue("@Status", "Pending");
+                            orderCommand.Parameters.AddWithValue("@OrderDate", DateTime.Now);
+                            var orderId = (int)await orderCommand.ExecuteScalarAsync();
+                            
+                            // Insert all validated items
+                            foreach (var validatedItem in validatedItems)
+                            {
+                                using (var itemCommand = new SqlCommand(itemQuery, connection, transaction))
+                                {
+                                    itemCommand.Parameters.AddWithValue("@OrderId", orderId);
+                                    itemCommand.Parameters.AddWithValue("@ProductId", validatedItem.ProductId);
+                                    itemCommand.Parameters.AddWithValue("@Quantity", validatedItem.Quantity);
+                                    itemCommand.Parameters.AddWithValue("@PriceAtOrder", validatedItem.PriceAtOrder);
+                                    await itemCommand.ExecuteNonQueryAsync();
+                                }
+                            }
+
+                            //Committing the Transaction to make changes permanent
+                            transaction.Commit();
+
+                            createOrderResponseDTO.Status = "Pending";
+                            createOrderResponseDTO.IsCreated = true;
+                            createOrderResponseDTO.OrderId = orderId;
+                            createOrderResponseDTO.Message = "Order Created Successfully";
+                            return createOrderResponseDTO;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        //This will rollback the transaction and discard all the changes we made into the Database
+                        transaction.Rollback();
+
+                        throw; // Re-throw to handle the exception further up the call stack
+                    }
+                }
+            }
+        }
     }
 }
