f9892a6716d2cbfff091d07872e5bfde17667b15
Karnasinh <karnasinh281@gmail.com>
2024-05-01 01:39:36 -0400

    Adds PaymentResponseDTO class to holds the response details whenver a new payment is registered for the Order. E.g. Payment is registered or not.

---
+++ ECommerceAPI/DTO/PaymentResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerceAPI.DTO
+{
+    //This class holds the response details whenver a new payment is registered for the Order.
+    //E.g. Payment is registered or not.
+    public class PaymentResponseDTO
+    {
+        public int PaymentId { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public bool IsCreated { get; set; }
+    }
+}
