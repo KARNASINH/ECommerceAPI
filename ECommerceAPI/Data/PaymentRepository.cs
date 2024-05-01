a856c19b24a0d38e2eb00b53a61e04686036aa32
Karnasinh <karnasinh281@gmail.com>
2024-05-01 18:31:00 -0400

    Injecting object of SqlConnectionFactory class using DI.

---
+++ ECommerceAPI/Data/PaymentRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceAPI.Data
+{
+    public class PaymentRepository
+    {
+        private readonly SqlConnectionFactory _connectionFactory;
+        public PaymentRepository(SqlConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+    }
+}
