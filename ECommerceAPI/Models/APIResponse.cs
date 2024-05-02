95731ee45e7ce03e827186bdd51723360ef76423
Karnasinh <karnasinh281@gmail.com>
2024-05-01 23:59:21 -0400

    Adds APIResponse<T> class to return the Same API response for both Successful or Unsuccessful result. This is a generic class so it can accept all type Data Type.

---
+++ ECommerceAPI/Models/APIResponse.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ECommerceAPI.Models
+{
+    public class APIResponse<T>
+    {
+        public bool Success { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public string Message { get; set; }
+        public T? Data { get; set; }
+        public object? Error { get; set; } //Data type is object so that it can accept any kind of Errors
+
+        public APIResponse(T data, string message ="", HttpStatusCode statusCode = HttpStatusCode.OK)
+        {
+            Success = true;
+            StatusCode = statusCode;
+            Message = message;
+            Data = data;
+            Error = null;
+        }
+
+        public APIResponse(HttpStatusCode statusCode ,string message, object error = null)
+        {
+            Success = false;
+            StatusCode = statusCode;
+            Message = message;
+            Data = default(T);
+            Error = null;
+        }
+    }
+}
