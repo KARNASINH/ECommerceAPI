a9f1c229f9b27d73b0e3c77608c47db7bbd9e563
Karnasinh <karnasinh281@gmail.com>
2024-05-02 00:11:58 -0400

    Adds code to inject CustomerRepository object using DI Design Pattern.

--- ECommerceAPI/Controllers/CustomerController.cs
+++ ECommerceAPI/Controllers/CustomerController.cs
@@ -6,6 +6,11 @@ namespace ECommerceAPI.Controllers
     //This controllers contains all the API End points related to Customer CRUD and other Operations
     public class CustomerController : Controller
     {
-        
+        //Injecting CustomerRepository object using DI Design Pattern
+        private readonly CustomerRepository _customerRepository;
+        public CustomerController(CustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
     }
 }
