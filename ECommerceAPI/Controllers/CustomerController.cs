0ce893944e284cb2e43f2b29c754a91810a557a6
Karnasinh <karnasinh281@gmail.com>
2024-05-02 00:11:31 -0400

    Adds CustomerController Controller which contains all the API End points related to Customer CRUD and other Operations.

---
+++ ECommerceAPI/Controllers/CustomerController.cs
@@ -0,0 +1,11 @@
+﻿using ECommerceAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.Controllers
+{
+    //This controllers contains all the API End points related to Customer CRUD and other Operations
+    public class CustomerController : Controller
+    {
+        
+    }
+}
