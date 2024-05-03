ac3eb41279e2e2ff3df3bd1da9caa7a658b9f84b
Karnasinh <karnasinh281@gmail.com>
2024-05-03 16:44:33 -0400

    Adds GetCustomerByIdAsync() end point to retrive the Customer details based on the given Customer Id.

--- ECommerceAPI/Controllers/CustomerController.cs
+++ ECommerceAPI/Controllers/CustomerController.cs
@@ -17,8 +17,8 @@ namespace ECommerceAPI.Controllers
             _customerRepository = customerRepository;
         }
 
-        //This action method return All Customer. 
-        // GET: api/customer
+        //This end point return All Customers. 
+        //GET: api/customer
         [HttpGet]
         public async Task<APIResponse<List<Customer>>> GetAllCustomers()
         {
@@ -35,5 +35,32 @@ namespace ECommerceAPI.Controllers
                 return new APIResponse<List<Customer>>(HttpStatusCode.InternalServerError, "Internal server error: " + ex.Message);
             }
         }
+
+        //This end point retrives the Customer details based on the given Customer Id.
+        //GET: api/customer/1
+        [HttpGet("{id}")]
+        public async Task<APIResponse<Customer>> GetCustomerById(int id)
+        {
+            try
+            {
+                //Fetches the Customer data from the database.
+                var customer = await _customerRepository.GetCustomerByIdAsync(id);
+
+                if(customer == null)
+                {
+                    //If customer not found in the Database it returns the reponse with 404 Http Status code.
+                    return new APIResponse<Customer>(HttpStatusCode.NotFound,"Customer not found for the given Custoemr Id.");
+                }
+
+                //If customer founds then return the reponse with 200 Http Statu code.
+                return new APIResponse<Customer>(customer, "Custome retrived successfully.");
+            }
+            catch (Exception ex) 
+            {
+                //If any exception occured then it retuns the reponse with 500 Http status code.
+                return new APIResponse<Customer>(HttpStatusCode.InternalServerError, "Internal server error: " + ex.Message);
+            }
+        }
+
     }
 }
