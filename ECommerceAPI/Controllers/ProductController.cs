278f47de4fa3d422f5cfbb6fb2f310d675c0a8d1
Karnasinh <karnasinh281@gmail.com>
2024-05-04 11:35:40 -0400

    Adds code to inject ProductRepository object using DI Design Pattern.

--- ECommerceAPI/Controllers/ProductController.cs
+++ ECommerceAPI/Controllers/ProductController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceAPI.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceAPI.Controllers
 {
+    //This Product Controller contains all the API End points related to Product CRUD and other Operations.
     public class ProductController : ControllerBase
     {
-        
+        //Injecting ProductRepository object using DI Design Pattern
+        private readonly CustomerRepository _customerRepository;
+        public ProductController(CustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
     }
 }
