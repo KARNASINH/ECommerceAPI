4f7821a083a1c35ac37d5ddfcc46c5fe5599096b
Karnasinh <karnasinh281@gmail.com>
2024-05-04 16:23:43 -0400

    Adds code to inject OrderRepository object using DI Design Pattern.

--- ECommerceAPI/Controllers/OrderController.cs
+++ ECommerceAPI/Controllers/OrderController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceAPI.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceAPI.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class OrderController : ControllerBase
     {
-       
+        //Injecting OrderRepository object using DI Design Pattern.
+        private readonly OrderRepository _orderRepository;
+        public OrderController(OrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
     }
 }
