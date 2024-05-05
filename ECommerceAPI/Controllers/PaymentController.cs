6418b6d6ebe0108eda40173c0b1968f98049f190
Karnasinh <karnasinh281@gmail.com>
2024-05-05 10:42:14 -0400

    Adds PaymentController Controller which contains all the API End points related to Payment CRUD and other Operations.

---
+++ ECommerceAPI/Controllers/PaymentController.cs
@@ -0,0 +1,11 @@
+﻿using ECommerceAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.Controllers
+{
+    //This class holds all the Payment related CURD and otehr operation's End Points.
+    public class PaymentController : ControllerBase
+    {
+        
+    }
+}
