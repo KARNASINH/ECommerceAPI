ca9b8a55bdbbf2e0e7787cc2f701e9cc42da90ff
Karnasinh <karnasinh281@gmail.com>
2024-05-05 10:42:37 -0400

    Adds code to inject PaymentRepository object using DI Design Pattern.

--- ECommerceAPI/Controllers/PaymentController.cs
+++ ECommerceAPI/Controllers/PaymentController.cs
@@ -6,6 +6,11 @@ namespace ECommerceAPI.Controllers
     //This class holds all the Payment related CURD and otehr operation's End Points.
     public class PaymentController : ControllerBase
     {
-        
+        //Injecting OrderRepository object using DI Design Pattern.
+        private readonly PaymentRepository _paymentRepository;
+        public PaymentController(PaymentRepository paymentRepository)
+        {
+            _paymentRepository = paymentRepository;
+        }
     }
 }
